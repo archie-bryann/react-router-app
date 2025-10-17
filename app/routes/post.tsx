815750850a96bd74b2474031e1098d55cb41b932
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-10-17 10:10:05 +0100

    useFetcher

--- app/routes/post.tsx
+++ app/routes/post.tsx
@@ -1,5 +1,10 @@
 import { useEffect } from "react";
 import type { Route } from "./+types/post";
+import { Form, redirect, useFetcher } from "react-router";
+
+// Server side loading and action
+// export async function loader() {}
+// export async function action() {}
 
 export async function clientLoader({ params }: Route.LoaderArgs) {
   // not server side loading
@@ -15,13 +20,45 @@ export function HydrateFallback() {
   return <p>Loading Game...</p>;
 }
 
-export async function action() {}
+// export async function clientAction({ params }: Route.LoaderArgs) {
+//   await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`, {
+//     method: "DELETE",
+//   });
+//   return redirect("/");
+// }
+
+export async function clientAction({ params }: Route.LoaderArgs) {
+  try {
+    await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`, {
+      method: "DELETE",
+    });
+    return { isDeleted: true };
+  } catch (error) {
+    return { isDeleted: false };
+  }
+}
 
 export default function Post({ loaderData }: Route.ComponentProps) {
+  const fetcher = useFetcher();
+
+  const isDeleted = fetcher.data?.isDeleted;
+
   return (
     <div>
-      <p>Post Title: {loaderData.title}</p>
-      <p>Post Body: {loaderData.body}</p>
+      {!isDeleted && (
+        <>
+          <p>Post Title: {loaderData.title}</p>
+          <p>Post Body: {loaderData.body}</p>
+        </>
+      )}
+
+      {/* <Form method="delete">
+        <button type="submit">Delete</button>
+      </Form> */}
+
+      <fetcher.Form method="delete">
+        <button type="submit">Delete</button>
+      </fetcher.Form>
     </div>
   );
 }
